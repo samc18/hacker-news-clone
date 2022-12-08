daefa8494e583fdd8b3075873204ab09f433fbe4
Sam Candela <byfyos72@gmail.com>
2022-12-08 17:17:04 -0500

    feat: add underline to active link

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 import RouterHandler from "./router.js";
 
+window.onhashchange = () => setActiveLink()
+
+function setActiveLink() {
+    const links = document.querySelectorAll('.header__link')
+    links.forEach(link => {
+        const linkPath = link.getAttribute('href')
+        const currentPath = window.location.hash
+        console.log(currentPath)
+        if (linkPath === currentPath) {
+            link.classList.add('active')
+        } else {
+            link.classList.remove('active')
+        }
+    })
+}
+
 class App {
     constructor() {
         new RouterHandler()
