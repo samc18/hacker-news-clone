2a5bdc6bf8fd610f5832c52ae33449493d8f353a
Sam Candela <byfyos72@gmail.com>
2022-12-09 11:19:47 -0500

    fix: solve issue with parsing special characters

--- components/Story.js
+++ components/Story.js
@@ -5,14 +5,14 @@ function Story(story) {
         <div class="story">
             <div class="story__line">
                 <span class="story__index">${index + 1 || ""}</span>
-                <span class"story__upvote">▲</span>
+                <span class="story__upvote">▲</span>
                 <a href=${url} class="story__title">${title}</a>
                 ${url ? `<span class="story__domain">(${domain && domain.slice(8, domain.length - 1)})</span>` : ''}
             </div>
             <div class="story__line">
                 <span class="story__info">${score} points by ${by} ${new Date(time).getHours()} hours ago</span> 
                 | <a href="#/item?id=${id}" class="story__comments">${descendants ? descendants : 0} comments</a>
-                | <button class="story__favorite" data-story='${JSON.stringify(story)}'>
+                | <button class="story__favorite" data-story='${JSON.stringify({...story, text: "", title: story.title.replace(/'/g,"&#x27;")})}'>
                     ${isFavorite ? 'Remove favorite' : 'Add to favorites'}
                 </button>
             </div>
